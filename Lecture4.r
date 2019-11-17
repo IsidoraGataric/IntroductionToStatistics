26aade283bb76422e79f566f31973bf2ed8b8944
Isidora Gatarić <gataric.isidora@gmail.com>
2019-11-17 22:39:07 +0100

    Update Lecture4.r

--- Lecture4.r
+++ Lecture4.r
@@ -1,7 +1,6 @@
 ######################### Descriptive statistics
 
 # Installing the packages.
-
 install.packages("car")
 install.packages("amap")
 install.packages("aod")
@@ -18,7 +17,6 @@ install.packages("vcd")
 install.packages("corpora")
 
 # Loading data set.
-
 UHM <- read.delim(file.choose())
 str(UHM)
 # 'data.frame':   1000 obs. of  5 variables:
@@ -30,14 +28,12 @@ str(UHM)
 attach(UHM)
 
 # Simple TABLE function allows us to get an insight into the number of data for each category of variables from the data set (that variable FILLER (uh, uhm, silence)).
-
 table(FILLER)
 # FILLER
 # silence      uh     uhm 
 #     332     394     274 
 
 # If you want to get percentages, then divided table(FILLER) with the length of the same variable (length (FILLER)), or just insret prop.table(table(FILLER)).
-
 table(FILLER)/length(FILLER)
 # FILLER
 # silence      uh     uhm 
@@ -54,7 +50,6 @@ prop.table(table(FILLER))
 plot(FILLER)
 
 # Example 2. 
-
 a <- c(1, 3, 5, 2, 4)
 b <- 1:5
 
@@ -72,7 +67,6 @@ plot(b, a, type="l")
 plot(b, a, xlab="A vector b", ylab="A vector b", xlim=c(0, 8), ylim=c(0,8), type="b")
 
 # Different types of graphics.
-
 m <- 1:5
 n <- 5:1
 x <- 6:10
@@ -93,7 +87,6 @@ pie(table(FILLER), col=c("grey20", "grey50", "grey80"))
 ######################### Bar plot
 
 # Useful when you have nominal/ordinal variable(s).
-
 barplot(table(FILLER), col=c("gray40", "grey60", "grey80"), names.arg=c("Silence", "uh", "uhm"), space=0)
 text(c(0.5, 1.5, 2.5), table(FILLER)/2, labels=table(FILLER))
 
@@ -111,7 +104,6 @@ lines(density(LENGTH))
 ######################### Measures of central tendency
 
 # Mode.
-
 which.max(table(FILLER))
 # uh 
 #  2 
@@ -120,19 +112,16 @@ max(table(FILLER))
 # [1] 394
 
 # Median.
-
 median(LENGTH)
 # [1] 897
 
 # Mean.
-
 mean(LENGTH)
 # [1] 915.043
 
 ######################### Dispersion measures
 
 # Never interpret CT measures without some dispersion measures. What measure have we mentioned so far ?! For each data type (and each MCT) there is some appropriate dispersion measure.
-
 Mean -- SD/variance.
 Mode -- relative entropy.
 Median -- quartiles.
@@ -143,18 +132,15 @@ mean(town1)
 mean(town2)
 
 # Relative entropy (each entropy is marked with H).
-
 article <- c(164, 33, 103)
 perc <- article/sum(article)
 hrel <-- sum(perc*log(perc))/log(length(perc))
 hrel
 
 # Range.
-
 range(LENGTH)
 
 # Qaurtile/Quantile.
-
 a <- 1:100
 #  0%  25%  50%  75% 100% 
 #   1   25   50   75  100
@@ -164,23 +150,17 @@ quantile(town2)
 # 6.00  7.75  9.00 11.25 16.00 
 
 # The larger difference between 1 and 4 quarters is, the Heterogeneous group is. 
-
 # Interpretation:
-
 quantile(LENGTH, probs=c(0.25, 0.5, 0.75, 1))
 
 # 25%  50%  75% 100% 
 # 583  897 1242 1600 
 
-# SD (Standard deviation).
-
-# A measure that tells us how many individual observations deviate from AS, this measure is squared, then all the values obtained are summed and eventually divided by n-1 (Bassel's correction).
-
+# SD (Standard deviation). This is a measure that tells us how many individual observations deviate from AS, this measure is squared, then all the values obtained are summed and eventually divided by n-1 (Bassel's correction).
 sd(town1)
 sd(town2)
 
 # Step by step.
-
 town1
 town1-mean(town1)
 (town1-mean(town1))^2
@@ -188,21 +168,16 @@ sum((town1-mean(town1))^2)
 sum((town1-mean(town1))^2)/(length(town1)-1)
 sqrt(sum((town1-mean(town1))^2)/(length(town1)-1))
 
-# Variance.
-
-# It counts over SD (and represents normalized SD, so we can compare it with other data sets).
-
+# Variance. This measure counts over SD (and represents normalized SD, so we can compare it with other data sets).
 sd(town1)/mean(town1)
 sd(town1*10)/mean(town1*10)
 
 # Summary function.
-
 summary(town1)
 
 #   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 # -12.00    4.00   13.50   10.25   18.50   23.00
 
 # Boxplots (tell us about dispersion of the data).
-
 boxsum <- boxplot(town1, town2, notch=TRUE, names=c("Town1", "Town2"))
 text(1:2, c(mean(town1), mean(town2)), c("+", "+"))
